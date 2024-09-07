797c13cf09b356d4406b6468f548da6ee368298e
3446798488 <3446798488@qq.com>
2024-09-07 03:42:03 +0000

    feat: 添加 i18n 抽取脚本

---
+++ scripts/i18n-match.mjs
@@ -0,0 +1,35 @@
+import { glob } from 'glob'
+import {createDefaultConfig} from 'i18nation/dist/index.js'
+import { sourceCodeReplacer } from 'i18nation/dist/sourceCodeReplacer.js'
+import {JSXPresets} from 'i18nation/dist/presets/jsx.js'
+import fs from 'fs'
+const items = glob.sync('src/**/*.{tsx,astro}')
+const context = {
+    json: {},
+    createTranslateCode(hash, params) {
+        return `$t("${hash}"${params ? `, ${params}` : ''})`
+    },
+    createStringSlot(key) {
+        return `{${key}}`
+    }
+}
+
+
+for (const item of items) {
+    console.log(item)
+  const content = await fs.promises.readFile(item, 'utf-8')
+  try{
+
+      const result = await sourceCodeReplacer(item,content, createDefaultConfig({
+          entry: [],
+          ...JSXPresets({
+              filename: item,
+              ...context
+            })
+        }))
+        fs.writeFileSync(item, result)
+    }catch(e){
+
+    }
+}
+fs.readFile()
